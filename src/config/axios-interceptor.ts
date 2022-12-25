80ef8b508fdd023783c4e0c353dd92c72356e755
simonmarcos <marcoseduardosimon@gmail.com>
2022-12-25 17:13:28 -0300

    - Added LoginModel - Added LoginSlice - Added login page with login logic

--- src/config/axios-interceptor.ts
+++ src/config/axios-interceptor.ts
@@ -4,5 +4,5 @@ const TIMEOUT = 1 * 60 * 1000;
 axios.defaults.timeout = TIMEOUT;
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "http://localhost:8080/",
 });
--- src/entities/login/Login.tsx
+++ src/entities/login/Login.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import LoginComponent from "../../components/login";
+import { ILoginModel } from "../../shared/model/Login";
+import { singInAplications as singInApplications } from "../../store/slices/loginSlice";
+import { AppDispatch } from "../../store/store";
 
 const LoginPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const formData = new FormData(event.currentTarget);
+
+    const data: ILoginModel = {
+      email: formData.get("email")?.toString(),
+      password: formData.get("password")?.toString(),
+    };
+
+    dispatch(singInApplications(data));
   };
 
   return <LoginComponent handleSubmit={handleSubmit} />;
