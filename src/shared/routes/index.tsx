d297bc53ab8a97a36c957454c28d83719f6da827
simonmarcos <marcoseduardosimon@gmail.com>
2022-12-26 00:04:09 -0300

    Added not found page

--- src/shared/routes/index.tsx
+++ src/shared/routes/index.tsx
@@ -1,14 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../../pages/home";
 import LoginPage from "../../pages/login";
+import NotFoundPage from "../../pages/not-found";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage />,
+    errorElement: <NotFoundPage />,
   },
   {
     path: "/home",
     element: <HomePage />,
+    errorElement: <NotFoundPage />,
   },
 ]);
