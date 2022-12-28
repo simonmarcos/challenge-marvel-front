68ca787f25dbd803c696610a75ec73ae49862ca5
simonmarcos <marcoseduardosimon@gmail.com>
2022-12-28 11:05:00 -0300

    Added custom hook in character page

--- src/pages/character/character.tsx
+++ src/pages/character/character.tsx
@@ -1,26 +1,19 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import CharacterComponent from "../../components/character/character-component";
 import useFetchAPI from "../../shared/hooks/useFetchApi";
 import { ICharacterModel } from "../../shared/model/Character";
-import { AppDispatch, RootState } from "../../store/store";
 
 const CharacterPage = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const { data, isPending, isSuccess, error, execute } = useFetchAPI();
-
-  const characterEntity: ICharacterModel[] = useSelector(
-    (state: RootState) => state.characterSlice.characters
-  );
+  const { data, isPending, execute } = useFetchAPI();
+  const characterEntity: ICharacterModel[] = data;
 
   useEffect(() => {
-    // dispatch(getEntitiesForMarvelAPI({}));
     execute("get", "/character/findAllFromMarvelApi");
   }, []);
 
-  return !isPending ? (
+  return !isPending && characterEntity.length > 0 ? (
     <>
-      {data.map((character: any) => {
+      {characterEntity.map((character: ICharacterModel) => {
         return (
           <CharacterComponent
             key={`index ${character.id}`}
